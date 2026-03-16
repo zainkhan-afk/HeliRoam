572c022e7ec03fdc58028b563329a8fadc3dac29
Zain <zain.9496@gmail.com>
2026-03-16 16:45:11 +0500

    rendered basic block

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ import { createRenderer, handleResize } from "./core/renderer.js";
 import { createPhysicsWorld, stepPhysics } from "./core/physics.js";
 import { createScene } from "./core/scene.js";
 import { createCamera } from "./core/camera.js";
+import { Helicopter } from "./objects/Helicopter.js";
 
 const renderer = createRenderer();
 const scene = createScene();
 const physics = createPhysicsWorld();
 const camera  = createCamera();
 
+const helicopter = new Helicopter(scene, physics);
+
+let lifting = false;
+
+window.addEventListener("mousedown", () => lifting = true);
+window.addEventListener("mouseup", () => lifting = false);
 
 function animate(){
     requestAnimationFrame(animate);
+    if (lifting) {
+        helicopter.applyLift();
+    }
+    stepPhysics(physics);
+    helicopter.update();
     renderer.render(scene, camera);
 }
 
--- objects/Helicopter.js
+++ objects/Helicopter.js
@@ -1,5 +1,63 @@
+import * as THREE from "three";
+import * as CANNON from "https://cdn.skypack.dev/cannon-es";
+
 export class Helicopter {
-    constructor() {
-        
+
+    constructor(scene, world) {
+
+        this.scene = scene;
+        this.world = world;
+
+        this.mesh = this.createMesh();
+        this.body = this.createPhysicsBody();
+
+        scene.add(this.mesh);
+        world.addBody(this.body);
     }
+
+    createMesh() {
+
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+        const material = new THREE.MeshStandardMaterial({
+            color: 0x00ff00
+        });
+
+        const mesh = new THREE.Mesh(geometry, material);
+
+        return mesh;
+    }
+
+    createPhysicsBody() {
+
+        const shape = new CANNON.Box(new CANNON.Vec3(0.5,0.5,0.5));
+
+        const body = new CANNON.Body({
+            mass: 1,
+            shape: shape,
+            position: new CANNON.Vec3(0,5,0)
+        });
+
+        body.linearDamping = 0.4;
+
+        return body;
+    }
+
+    update() {
+
+        // sync physics → graphics
+        this.mesh.position.copy(this.body.position);
+        this.mesh.quaternion.copy(this.body.quaternion);
+
+    }
+
+    applyLift(force = 40) {
+
+        this.body.applyForce(
+            new CANNON.Vec3(0, force, 0),
+            this.body.position
+        );
+
+    }
+
 }
