539cda59bfe634dbe6462e3fd006f3830ff2b70f
Zain <zain.9496@gmail.com>
2026-06-17 19:55:55 +0500

    Added force test bed

--- test/index.js
+++ test/index.js
@@ -1,5 +1,26 @@
 import * as THREE from "three";
 import { OrbitControls} from "jsm/controls/OrbitControls.js"
+import * as CANNON from "cannon";
+
+function DirectionalLight() {
+  const dirLight = new THREE.DirectionalLight();
+  dirLight.position.set(-100, -100, 200);
+  dirLight.up.set(0, 0, 1);
+  dirLight.castShadow = true;
+
+  dirLight.shadow.mapSize.width = 2048;
+  dirLight.shadow.mapSize.height = 2048;
+
+  dirLight.shadow.camera.up.set(0, 0, 1);
+  dirLight.shadow.camera.left = -400;
+  dirLight.shadow.camera.right = 400;
+  dirLight.shadow.camera.top = 400;
+  dirLight.shadow.camera.bottom = -400;
+  dirLight.shadow.camera.near = 50;
+  dirLight.shadow.camera.far = 400;
+
+  return dirLight;
+}
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -9,47 +30,102 @@ renderer.setSize(w, h);
 
 document.body.appendChild(renderer.domElement);
 
-const fov = 75;
+const fov = 100;
 const aspect = w / h;
 const near = 0.1;
-const far = 10;
+const far = 100;
 
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
 camera.position.z = 2;
+camera.position.x = 10;
+camera.position.x = 10;
+
+camera.up.set(0, 0, 1);
+camera.lookAt(0, 0, 0);
 
 const scene = new THREE.Scene();
+const world = new CANNON.World();
+
+// world.gravity.set(0, 0, -9.82);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
-const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshStandardMaterial({
-    color: 0xccff,
+const body = new CANNON.Body({
+    mass: 1,
+    shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
+    position: new CANNON.Vec3(0,0,1)
+});
+
+// body.linearDamping = 0.4;
+// body.angularDamping = 0.9;
+
+const geo = new THREE.BoxGeometry(1, 1, 1);
+const mat = new THREE.MeshLambertMaterial({
+    color: 0xccff00ff,
     flatShading: true
 });
 
+const floorGeo = new THREE.PlaneGeometry(100, 100);
+const floorMat = new THREE.MeshLambertMaterial({
+    color: 0xffffff,
+    flatShading: true,
+});
+
+const floorMesh = new THREE.Mesh(floorGeo, floorMat);
+scene.add(floorMesh);
+
+
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
+world.addBody(body);
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
-scene.add(hemiLight);
 
-const wireMat = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    wireframe: true
-})
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
 
-const wireMesh = new THREE.Mesh(geo, wireMat);
-mesh.add(wireMesh);
+const dirLight = DirectionalLight();
+scene.add(dirLight);
+
+// const wireMesh = new THREE.Mesh(geo, wireMat);
+// mesh.add(wireMesh);
+
+body.angularVelocity = new CANNON.Vec3(0.1, 0, 0);
+// body.torque = new CANNON.Vec3(10, 0, 10);
+
+const keys = {
+    w: false,
+    a: false,
+    s: false,
+    d: false,
+};
+
+window.addEventListener("keydown", (e) => {
+    const key = e.key.toLowerCase();
+    if (key in keys) {
+        keys[key] = true;
+    }
+});
+
+window.addEventListener("keyup", (e) => {
+    const key = e.key.toLowerCase();
+    if (key in keys) {
+        keys[key] = false;
+    }
+});
 
 function animate(t = 0){
     requestAnimationFrame(animate);
     // mesh.scale.setScalar(Math.cos(t*0.001)/2 + 1.0);
     // geo.rotateZ(0.01);
+    mesh.position.copy(body.position);
+    mesh.quaternion.copy(body.quaternion);
+    
     renderer.render(scene, camera);
     controls.update();
+    world.step(0.1);
     
     t += 1
 }
